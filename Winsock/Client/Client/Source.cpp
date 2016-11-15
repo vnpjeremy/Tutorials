7e66e459834b41aefcd45c90cefdefc48c22e4bd
Viper-PC\Viper <kreppsjs@gmail.com>
2016-11-14 20:13:50 -0600

    start winsock

--- Winsock/Client/Client/Source.cpp
+++ Winsock/Client/Client/Source.cpp
@@ -1,6 +1,17 @@
 /* Client */
+#pragma comment(lib, "ws2_32.lib")
+#include <WinSock2.h>
+#include <iostream>
 
 int main()
 {
+    WSAData wsadata;                     //Populate this by starting up winsock
+    WORD    dllversion = MAKEWORD(2, 1); //Ugh okay so WORD == unsigned short. Useless typedef.
+    if(WSAStartup(dllversion, &wsadata) != 0)
+    {
+        MessageBoxA(NULL, "Winsock startup failed", "Error", MB_OK | MB_ICONERROR);
+        exit(1);
+    }
 
+    return 0;
 }
